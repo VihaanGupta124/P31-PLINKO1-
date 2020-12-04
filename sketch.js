aae4db028ab4f6d823c1c2819aa2d44f043c9251
VihaanGupta124 <69687681+VihaanGupta124@users.noreply.github.com>
2020-12-04 13:18:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+ 
+var particles = [];
+var plinkos = [];
+var divisions =[];
+var divisionHeight=300;
+var score =0;
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2))
+   }
+  }
+  
